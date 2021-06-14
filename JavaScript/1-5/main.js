7f52c7bb0d555fd6e0d86321bc9130cb158c3670
yukimura-willnect <80313750+yukimura-willnect@users.noreply.github.com>
2021-06-15 08:59:42 +0900

    課題提出1-5

---
+++ JavaScript/1-5/main.js
@@ -0,0 +1,7 @@
+function createJuice(fruits){
+    console.log(fruits +"を受け取りました。ジュースにして返します");
+    return fruits+"ジュース";
+}
+
+let orangeJuice = createJuice("みかん");
+console.log(orangeJuice + "が届きました");
