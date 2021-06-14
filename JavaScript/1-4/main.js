ef70f0d235e5e897e0f370353d2e3be28979a2e0
yukimura-willnect <80313750+yukimura-willnect@users.noreply.github.com>
2021-06-15 08:14:06 +0900

    課題提出１−４

---
+++ JavaScript/1-4/main.js
@@ -0,0 +1,14 @@
+for(let number =1; number<=100;number++){
+
+    if (number%3===0 && number%5===0 ){
+        console.log('FizzBuzz!!');
+    } else if (number%5===0){
+        console.log('Buzz!');
+    } else if (number%3===0){
+        console.log('Fizz!');
+    } else {
+        console.log(number);
+    }
+
+    
+}
